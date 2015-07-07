fe7bfd976b7ec83dbf0fd73584e0607f49976b7f
tmurray <tmurray@wumpus-cave.net>
2015-07-06 19:57:56 -0500

    Put caps back in, but without framerate

--- webcam.pl
+++ webcam.pl
@@ -19,6 +19,9 @@ sub bus_callback
         warn "End of stream, quitting\n";
         $loop->quit;
     }
+    else {
+        warn "Unknown message: " . $msg->type;
+    }
 
     return TRUE;
 }
@@ -30,17 +33,17 @@ sub bus_callback
 
     my $pipeline = GStreamer1::Pipeline->new( 'pipeline' );
     my $v4l2src = GStreamer1::ElementFactory::make( v4l2src => 'and_who');
-    #my $capsfilter = GStreamer1::ElementFactory::make(
-    #    capsfilter => 'are_you' );
+    my $capsfilter = GStreamer1::ElementFactory::make(
+        capsfilter => 'are_you' );
     my $xvimagesink = GStreamer1::ElementFactory::make(
         xvimagesink => 'the_proud_lord_said' );
 
-    #my $caps = GStreamer1::Caps::Simple->new( 'video/x-raw',
-    #    width     => 'Glib::Int'    => 640,
-    #    height    => 'Glib::Int'    => 480,
-    #    framerate => 'Glib::Double' => (30/1),
-    #);
-    #$capsfilter->set( caps => $caps );
+    my $caps = GStreamer1::Caps::Simple->new( 'video/x-raw',
+        width     => 'Glib::Int'    => 640,
+        height    => 'Glib::Int'    => 480,
+        #framerate => 'Glib::Double' => (30/1),
+    );
+    $capsfilter->set( caps => $caps );
 
     $v4l2src->set(
         device => INPUT,
@@ -48,12 +51,11 @@ sub bus_callback
 
     $pipeline->add( $_ ) for
         $v4l2src,
-        #$capsfilter,
+        $capsfilter,
         $xvimagesink;
 
-    $v4l2src->link( $xvimagesink );
-    #$v4l2src->link( $capsfilter );
-    #$capsfilter->link( $xvimagesink );
+    $v4l2src->link( $capsfilter );
+    $capsfilter->link( $xvimagesink );
 
     my $bus = $pipeline->get_bus;
     $bus->add_signal_watch;
