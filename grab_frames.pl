e4fbd0ee54b117860081f387891ef0f75d72213d
tmurray <tmurray@wumpus-cave.net>
2015-07-09 17:15:12 -0500

    Parse h264 frames and extract them from the GstBuffer

--- grab_frames.pl
+++ grab_frames.pl
@@ -3,6 +3,7 @@ use v5.14;
 use warnings;
 use GStreamer1;
 use Glib qw( TRUE FALSE );
+use Digest::Adler32::XS;
 
 my $FILE = shift or die "Need a file to process\n";
 
@@ -26,8 +27,14 @@ sub bus_eos_callback
 sub handoff_callback
 {
     my ($fakesink, $buffer, $pad) = @_;
-DEBUG: $DB::single = 1;
-    say "Got frame, size: " . $buffer->get_size;
+    my $size = $buffer->get_size;
+    my $frame_data = $buffer->extract_dup( 0, $size );
+
+    my $digest = Digest::Adler32::XS->new;
+    $digest->add( @$frame_data );
+    my $checksum = $digest->hexdigest;
+
+    say "Got frame, size: $size, checksum: $checksum";
     return TRUE;
 }
 
@@ -38,21 +45,36 @@ DEBUG: $DB::single = 1;
 
     my $pipeline = GStreamer1::Pipeline->new( 'pipeline' );
     my $filesrc = GStreamer1::ElementFactory::make( filesrc => 'and_who');
+    my $capsfilter = GStreamer1::ElementFactory::make(
+        capsfilter => 'are_you' );
+    my $h264parse = GStreamer1::ElementFactory::make(
+        'h264parse' => 'the_proud_lord_said' );
     my $fakesink = GStreamer1::ElementFactory::make(
-        fakesink => 'are_you' );
+        fakesink => 'that_i_should_bow_so_low' );
+
 
     $filesrc->set(
         location => $FILE,
     );
+    my $caps = GStreamer1::Caps::Simple->new( 'video/x-h264',
+        width     => 'Glib::Int'    => 640,
+        height    => 'Glib::Int'    => 480,
+    );
+    $capsfilter->set( caps => $caps );
+
     $fakesink->set(
         'signal-handoffs' => TRUE,
     );
 
     $pipeline->add( $_ ) for
         $filesrc,
+        $capsfilter,
+        $h264parse,
         $fakesink;
 
-    $filesrc->link( $fakesink );
+    $filesrc->link( $capsfilter );
+    $capsfilter->link( $h264parse );
+    $h264parse->link( $fakesink );
 
     my $bus = $pipeline->get_bus;
     $bus->add_signal_watch;
